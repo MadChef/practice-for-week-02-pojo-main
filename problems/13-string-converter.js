bca8fd4d44c507593dd6d3a1aa05274969fd7927
MadChef <d@LAPTOP-2967894C>
2023-09-13 13:57:20 -0400

    test 12 failing but functioning

--- problems/13-string-converter.js
+++ problems/13-string-converter.js
@@ -13,6 +13,18 @@ console.log(stringConverter("raccoon")); // => {r: 1, a: 1, c: 2, o: 2, n: 1}
 
 function stringConverter(string) {
   // Your code here
+  let output = [];
+  for (let i = 0; i < string.length; i++){
+    if (output[string[i]] == undefined){
+      output[string[i]] = 1;
+    }else{
+     // console.log([string[i]] + " exists " +output[string[i]]);
+      output[string[i]] = output[string[i]] + 1;
+   //   console.log(output[string[i]]);
+    }
+  }
+  //  console.log(output);
+  return output;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
